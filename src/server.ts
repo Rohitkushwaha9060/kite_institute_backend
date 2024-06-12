c46e7134e0b3d9addf7b87fd0f60df8cb7cad936
Rohit Kushwaha <rajdilkhush0987654321@gmail.com>
2024-06-13 01:05:57 +0530

    socket io setup complete

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import http from 'http';
+import * as socketIO from 'socket.io';
 
 // file imports
 import app from '@/app';
@@ -6,6 +7,28 @@ import { logger, secrets } from '@/core';
 
 // server initialization
 const server = http.createServer(app);
+
+// socket.io initialization
+const io = new socketIO.Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+        optionsSuccessStatus: 204,
+        preflightContinue: true,
+    },
+});
+
+// set up socket.io
+io.on('connection', (socket) => {
+    logger.info('A user connected ' + socket.id);
+
+    socket.on('disconnect', () => {
+        logger.info('A user disconnected ' + socket.id);
+    });
+});
+
+// set io in app
+app.set('socket', io);
 
 // server start
 server.listen(secrets.PORT, () => {
